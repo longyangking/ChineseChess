35d4131daa35f0cb1ed3d4b493ad7e273f91bb81
Long Yang <longyang_123@yeah.net>
2016-10-28 10:24:48 +0800

    Basic Structure

---
+++ src/chinesechess/ChessConfig.java
@@ -0,0 +1,18 @@
+package chinesechess;
+
+public class ChessConfig{
+	//Chess Info
+	public static final int Rook = 1;
+	public static final int Knight = 2;
+	public static final int Elephant = 3;
+	public static final int Mandarin = 4;
+	public static final int King = 5;
+	public static final int Cannon = 6;
+	public static final int Pawn = 7;
+	
+	//Resource Info
+	
+	public static void ResourceInit(){
+		
+	}
+ }
